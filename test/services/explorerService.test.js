32df35ec7e8ae2314b717091435bf01579965f50
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-28 22:26:20 -0500

    Create explorerService.test.js

---
+++ test/services/explorerService.test.js
@@ -0,0 +1,9 @@
+const ExplorerService = require ("./../../lib/services/ExplorerService");
+
+describe ("Tests para ExplorerService", () => {
+    test("Requerimiento 1: Calcular Todos los Explorers en una misión", () => {
+        const explorers = [{mission : "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
+    });
+});

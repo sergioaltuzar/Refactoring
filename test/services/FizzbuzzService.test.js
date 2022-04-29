1933c3cedb5c29db24e09e88eb929db5694ac428
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-29 00:21:49 -0500

    instalando lint

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,25 +1,25 @@
 const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
 describe("Unit Test FizzbuzzService class", () => {
-    test('1) Score Value ' , () => {
+    test('1) Score Value Explorer1' , () => {
         const explorer1 = {name: "Explorer1", score: 1};
         FizzbuzzService.applyValidationInExplorer(explorer1); 
         expect(explorer1.trick).toBe(1);
     });
 
-    test('2) Fizz', () => {
+    test('2) Fizz Explorer3', () => {
         const explorer3 = {name: "Explorer3", score: 3};
         FizzbuzzService.applyValidationInExplorer(explorer3); 
         expect(explorer3.trick).toBe("FIZZ");
     });
 
-    test('3) Test Buzz', () => {
+    test('3) Test Buzz Explorer5', () => {
         const explorer5 = {name: "Explorer5", score: 5};
         FizzbuzzService.applyValidationInExplorer(explorer5); 
         expect(explorer5.trick).toBe("BUZZ");
     });
 
-    test('4) FitBuzz', () => {
+    test('4) FizzBuzz Explorer15', () => {
         const explorer15 = {name: "Explorer15", score: 15};
         FizzbuzzService.applyValidationInExplorer(explorer15); 
         expect(explorer15.trick).toBe("FIZZBUZZ");
